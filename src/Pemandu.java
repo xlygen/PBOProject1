1eb8ad85cf1e967288c1f1b5a60916049b860bc9
Dimas Rizky <dimasrizkyriadi95@gmail.com>
2024-12-20 12:57:21 +0700

    First

--- src/Pemandu.java
+++ src/Pemandu.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Pemandu extends User {
+    private String zonaBekerja;
+    private String Jadwal;
 
-public class Pemandu {
+    public Pemandu(int id, String nama, String zonaBekerja, String jadwal) {
+        super(id, nama);
+        this.zonaBekerja = zonaBekerja;
+        Jadwal = jadwal;
+    }
+
+    public String getZonaBekerja() {
+        return zonaBekerja;
+    }
+
+    public String getJadwal() {
+        return Jadwal;
+    }
+
+
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public abstract class User {
+    private int id;
+    private String nama;
 
-public class User {
+    public User(int id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+
 }
